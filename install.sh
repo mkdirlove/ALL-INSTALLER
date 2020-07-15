ce6f5d56b8a68a5fa8573f4a0ef2c2fa743b4f3d
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2020-07-15 20:06:09 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+###############################################################################################
+#                                   ALL IN ONE TOOL INSTALLER                                 #
+#                                                                                             #
+# AUTHOR   : JAYSON CABRILLAS SAN BUENAVENTURA                                                #
+# GITHUB   : https://github.com/mkdirlove                                                     #
+# FACEBOOK : https://web.facebook.com/mkdirlove.git                                           #
+#                                                                                             #
+###############################################################################################
+
+sudo apt update 
+sudo apt install whiptail
+mkdir TOOLS
